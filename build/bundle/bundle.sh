ae15cf198ceb5e77e111030dc3623b3d20f30660
Jurek <git@jurek.io>
2024-10-08 19:43:36 +0200

    building.sh

--- build/bundle/bundle.sh
+++ build/bundle/bundle.sh
@@ -9,12 +9,12 @@ if [ -z "$1" ]; then
 fi
 
 if [ -z "$CRAFTIP_SERVER" ]; then
-    echo "Error: No \$CRAFTIP_SERVER provided."
+    echo "Error: No \$CRAFTIP_SERVER provided. <username>@<server>"
     exit 1
 fi
 
 if [ -z "$CRAFTIP_SERVER_DIR" ]; then
-    echo "Error: No \$CRAFTIP_SERVER_DIR provided."
+    echo "Error: No \$CRAFTIP_SERVER_DIR provided. /var/www/update.craftip.net"
     exit 1
 fi
 
@@ -27,6 +27,10 @@ mkdir /tmp/craftip-bundle
 unzip $1 -d $BUNDLE/input
 
 cargo run -- --input $BUNDLE/input/bin --output $BUNDLE/output --ver `cat ${BUNDLE}/input/version`
-scp -r $BUNDLE/output/* "${CRAFTIP_SERVER}:${CRAFTIP_SERVER_DIR}/"
+scp -r $BUNDLE/output/* "${CRAFTIP_SERVER}:${CRAFTIP_SERVER_DIR}/update/v1/"
 cargo run -- --test-staging
-ssh ${CRAFTIP_SERVER} "mv ${CRAFTIP_SERVER_DIR}/latest.json.staging.json ${CRAFTIP_SERVER_DIR}/latest.json"
+scp -r $BUNDLE/input/downloads/ "${CRAFTIP_SERVER}:${CRAFTIP_SERVER_DIR}/downloads_staging"
+ssh ${CRAFTIP_SERVER} "\
+    mv ${CRAFTIP_SERVER_DIR}/update/v1/latest.json.staging.json ${CRAFTIP_SERVER_DIR}/update/v1/latest.json;\
+    rm -rf ${CRAFTIP_SERVER_DIR}/downloads;\
+    mv ${CRAFTIP_SERVER_DIR}/downloads_staging ${CRAFTIP_SERVER_DIR}/downloads"
