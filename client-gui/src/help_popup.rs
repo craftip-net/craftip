58cef06d609f8c0a9dbae9345e447b2a1bb850d0
Jurek <git@jurek.io>
2025-09-03 11:01:56 -0700

    removing deprecated warning

--- client-gui/src/help_popup.rs
+++ client-gui/src/help_popup.rs
@@ -35,7 +35,9 @@ impl HelpPopup {
                 ui.add_sized(Vec2::new(300.0, 10.0), label);
                 ui.label("");
 
-                ui.set_enabled(self.promise.is_none());
+                if self.promise.is_some() {
+                    ui.disable();
+                }
 
                 ui.label("Email for response (optional):");
                 ui.text_edit_singleline(&mut self.email);
